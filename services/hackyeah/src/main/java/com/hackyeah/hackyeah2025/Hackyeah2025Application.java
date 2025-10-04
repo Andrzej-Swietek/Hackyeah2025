3d1807f7d368a2a72dcbf13815c56bc3c27127d0
Andrzej Świętek <a.swietek@avsystem.com>
2025-10-04 13:01:49 +0200

    Added backend setup with dockers

---
+++ services/hackyeah/src/main/java/com/hackyeah/hackyeah2025/Hackyeah2025Application.java
@@ -0,0 +1,13 @@
+package com.hackyeah.hackyeah2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackyeah2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackyeah2025Application.class, args);
+	}
+
+}
